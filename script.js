a7571cde2610740f344e8b303d857290f310bb8c
Diyor <117651682+Dain55@users.noreply.github.com>
2023-02-07 15:08:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let counters = document.querySelectorAll("[data-counter]");
+
+if (counters) {
+  counters.forEach((counter) => {
+    counter.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target.closest(".counter__button")) {
+        let value = parseInt(target.closest('.counter').querySelector('input').value);
+
+
+       if (target.classList.contains('counter__button_plus')) {
+          value++
+       } else {
+          value--
+       }
+            
+       if (value <= 0) {
+           value = 0
+      
+       }
+        target.closest('.counter').querySelector('input').value = value
+       
+      }
+    });
+  });
+}
